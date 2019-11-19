d64e3beb894bab07cebe31e9b7b656a11abc8a33
unknown <krntranslation@gmail.com>
2019-11-19 14:34:57 -0600

    inital commit

---
+++ CommonDate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommonDate.Startup))]
+namespace CommonDate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
